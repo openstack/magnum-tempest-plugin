599a7c392d2fbb20085fbb115aae2337e4f64357
pengyuesheng <pengyuesheng@gohighsec.com>
2019-08-02 14:27:06 +0800

    Updated from global requirements
    --
    
    Bump the openstackdocstheme extension to 1.20
    
    Some options are now automatically configured by the version 1.20:
    - project
    - html_last_updated_fmt
    - latex_engine
    - latex_elements
    - version
    - release.
    
    Ic9207bb8d3e602dd71cc4c7cefde739130f3d48b
    
    --
    
    Blacklist sphinx 2.1.0 (autodoc bug)
    
    See https://github.com/sphinx-doc/sphinx/issues/6440 for upstream details
    
    Ia029f66cfde96d97754e38b5278f1b24081a09e0
    
    --
    
    tox: Keeping going with docs
    
    1. Sphinx 1.8 introduced [1] the '--keep-going' argument which, as its name
    suggests, keeps the build running when it encounters non-fatal errors.
    This is exceptionally useful in avoiding a continuous edit-build loop
    when undertaking large doc reworks where multiple errors may be
    introduced.
    
    2. Remove the py27 from tox since magnum already drop support python 2.7
    
    [1] https://github.com/sphinx-doc/sphinx/commit/e3483e9b045
    
    I6e713b51cf9fac87e696fdfce8156f656a0f1df5
    
    --
    
    Change-Id: I53800a688fae3ba9cc551b0ceffe96938f2df59d

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -52,7 +52,6 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'magnum_tempest_plugin Release Notes'
 copyright = u'2017, OpenStack Developers'
 
 # openstackdocstheme options
@@ -195,17 +194,6 @@ htmlhelp_basename = 'magnum_tempest_pluginReleaseNotesdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    # 'preamble': '',
-}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
