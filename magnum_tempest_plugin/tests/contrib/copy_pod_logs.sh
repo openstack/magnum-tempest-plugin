1e37dbafd4e3a6888204a2b14373908b6c39ae18
Michal Nasiadka <mnasiadka@gmail.com>
2024-02-07 14:48:24 +0100

    CI: Wait for pods to exit ContainerCreating state
    Change-Id: I038cfab481f3a30050f39883fce99973586597b0

--- magnum_tempest_plugin/tests/contrib/copy_pod_logs.sh
+++ magnum_tempest_plugin/tests/contrib/copy_pod_logs.sh
@@ -18,6 +18,17 @@ LOG_DIR="/tmp/magnum-nodes/kubernetes/"
 KUBECTL="/opt/stack/bin/kubectl --kubeconfig /tmp/magnum-nodes/kube.conf"
 mkdir -p ${LOG_DIR}
 
+while ${KUBECTL} get pods -A -o wide | grep -q "ContainerCreating";
+do
+    count=$(( $count+1 ))
+    if [ "$count" = "10" ]; then
+        echo "ERROR: Waiting for pods to exit ContainerCreating state timed out"
+        exit 1
+    else
+        sleep 60
+    fi
+done
+
 ${KUBECTL} get all -A -o wide > ${LOG_DIR}/kubectl_get_all
 
 for ns in $(${KUBECTL} get -o name namespace); do
@@ -26,3 +37,13 @@ for ns in $(${KUBECTL} get -o name namespace); do
         ${KUBECTL} -n ${ns#*/} logs ${pod#*/} > ${LOG_DIR}/pods/${ns#*/}/${pod#*/}
     done
 done
+
+# NOTE(mnasiadka): Fail if any pod is not Running
+FAILED_PODS=$(${KUBECTL} get pods -A --field-selector=status.phase!=Running)
+
+if [ ! -z "${FAILED_PODS}" ]; then
+    echo "ERROR: Detected pods in state other than Running"
+    echo ${FAILED_PODS}
+    exit 1
+fi
+
